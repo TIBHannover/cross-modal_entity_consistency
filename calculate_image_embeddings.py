0ff5ee3a59869cbbfe9b9eb533f5f3c30cad657d
Eric Mueller <eric.mueller@tib.eu>
2021-01-13 13:36:04 +0100

    improved method to find correct file extension for news images

--- calculate_image_embeddings.py
+++ calculate_image_embeddings.py
@@ -1,5 +1,6 @@
 import absl.logging
 import argparse
+import glob
 import h5py
 import logging
 import numpy as np
@@ -13,30 +14,20 @@ from visual_descriptors.scene_embedding import SceneClassificator
 
 def parse_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-v', '--info', action='store_true', help='info output')
-    parser.add_argument('-vv', '--debug', action='store_true', help='debug output')
-    
+    parser.add_argument('-v', '--debug', action='store_true', help='debug output')
+
     parser.add_argument('-i', '--input', type=str, required=True, help='path to input.jsonl')
     parser.add_argument('-d', '--directory', type=str, required=True, help='input folder containing the image data')
     parser.add_argument('-t', '--type', type=str, required=True, choices=['news', 'entity'], help='specify image type')
 
-    parser.add_argument('-o', '--output', type=str, required=True, help='Path to output directory')
-
     parser.add_argument('-m', '--model', type=str, required=True, help='Path to model directory')
+
+    parser.add_argument('-o', '--output', type=str, required=True, help='Path to output directory')
 
     parser.add_argument('--logits', action='store_true', help='set to get scene probabilities for places365')
     parser.add_argument('--use_cpu', action='store_true', help='set to run script on cpu')
     args = parser.parse_args()
     return args
-
-
-def find_file(filename, possible_file_extensions=['.jpg', '.png', '.webp']):
-    fname = None
-    for ext in possible_file_extensions:
-        if os.path.exists(filename + ext):
-            fname = filename + ext
-
-    return fname
 
 
 def main():
@@ -104,15 +95,17 @@ def main():
                 images.append({'fname': fname, 'search_engine': None})
             else:  # entity
                 for image in entry['image_urls']:
-                    fname, ext = os.path.splitext(image['filename'])
+                    fname, _ = os.path.splitext(image['filename'])
                     fname = os.path.join(args.directory, entry['wd_id'], fname)
                     images.append({'fname': fname, 'search_engine': image['search_engine']})
 
             for image in images:
-                image_file = find_file(image['fname'])
-                if not image_file:
+                image_files = glob.glob(image['fname'] + '.*')  # get image_file with extension
+                if len(image_files) == 0:
                     logging.info(f"Cannot find image file {image['fname']}.jpg")
                     continue
+                else:
+                    image_file = image_files[0]
 
                 if args.type == 'news':
                     h5_key = entry['id']
